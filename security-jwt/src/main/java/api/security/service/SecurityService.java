6aeb4076cc186ad99b41d534fa10a9b5228bd04a
VladMakhov <makhov1@list.ru>
2023-10-03 10:17:59 +0300

    unversioned files

---
+++ security-jwt/src/main/java/api/security/service/SecurityService.java
@@ -0,0 +1,58 @@
+package api.security.service;
+
+import api.security.model.User;
+import api.security.model.dto.LoginRequest;
+import api.security.model.dto.LoginResponse;
+import api.security.model.dto.RegistrationRequest;
+import api.security.repo.UserRepository;
+import api.security.util.JwtUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityService {
+
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityService(JwtUtil jwtUtil, AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public ResponseEntity<LoginResponse> login(LoginRequest request) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = jwtUtil.generateToken(authentication);
+
+        LoginResponse response = new LoginResponse();
+        response.setToken(token);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    public ResponseEntity<String> registration(RegistrationRequest request) {
+        if (userRepository.existsUserByUsername(request.getUsername())) {
+            return new ResponseEntity<>("Chose different username", HttpStatus.BAD_REQUEST);
+        }
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        userRepository.save(user);
+
+        return new ResponseEntity<>("User registered successfully", HttpStatus.OK);
+    }
+}
